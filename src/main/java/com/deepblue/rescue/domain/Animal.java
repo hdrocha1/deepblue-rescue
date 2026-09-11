eca1ffcb4dc59fdb036e10e73b3eb94512c9932e
Hewitt Rocha <hdrocha@unimagdalena.edu.co>
2026-09-11 00:43:46 -0500

    test: demonstrate ddl-auto validate failure

--- src/main/java/com/deepblue/rescue/domain/Animal.java
+++ src/main/java/com/deepblue/rescue/domain/Animal.java
@@ -38,7 +38,7 @@ public class Animal {
     @OneToMany(mappedBy = "animal", fetch = FetchType.LAZY)
     private List<Treatment> treatments = new ArrayList<>();
 
-    @Column(name = "tracking_device_code", length = 50, unique = true)
+    @Column(name = "gps_device", length = 50, unique = true)
     private String trackingDeviceCode;
 
     protected Animal() {}
